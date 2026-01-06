bce6d14b58912317bbb1b5b94b0ab8166d8e6832
ajay <ajay.l@intellectselect.com>
2026-01-06 20:43:59 +0530

    scope chain

--- 01-Fundamentals-Part-1/final/mine.js
+++ 01-Fundamentals-Part-1/final/mine.js
@@ -12,22 +12,53 @@
 // console.log(a)
 // console.log(b)
 
-getName()
-console.log(getName2)
-console.log(getName3)
-console.log(x)
-console.log(getName)
-getName2()
 
-var x = 7
-function getName() {
-  console.log("Javascript")
-}
 
-var getName2 = () => {
-  console.log("fun 2")
-}
+// getName()
+// console.log(getName2)
+// console.log(getName3)
+// console.log(x)
+// console.log(getName)
+// getName2()
 
-var getName3 = function () {
-  console.log("fun 3")
-}
+// var x = 7
+// function getName() {
+//   console.log("Javascript")
+// }
+
+// var getName2 = () => {
+//   console.log("fun 2")
+// }
+
+// var getName3 = function () {
+//   console.log("fun 3")
+// }
+
+
+// How function works
+// var x = 1
+// a()
+// b()
+// console.log(x)
+
+// function a() {
+//   var x = 10
+//   console.log(x)
+// }
+
+// function b() {
+//   var x = 100
+//   console.log(x)
+// }
+
+
+// Scope chain
+// function a() {
+//   var b = 10
+//   function c() {
+//     console.log(b)
+//   }
+//   c()
+// }
+
+// a()
