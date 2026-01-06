c5abce84fc02f673d6b35b21240de1f07148b340
ajay <ajay.l@intellectselect.com>
2026-01-06 15:26:49 +0530

    new practise file

---
+++ 01-Fundamentals-Part-1/final/mine.js
@@ -0,0 +1,33 @@
+
+// console.log(x)
+// var x = 7
+// let b
+// const a = null
+// {
+//   // b = "this"
+//   // const a = "AA"
+//   console.log(a)
+//   console.log(b)
+// }
+// console.log(a)
+// console.log(b)
+
+getName()
+console.log(getName2)
+console.log(getName3)
+console.log(x)
+console.log(getName)
+getName2()
+
+var x = 7
+function getName() {
+  console.log("Javascript")
+}
+
+var getName2 = () => {
+  console.log("fun 2")
+}
+
+var getName3 = function () {
+  console.log("fun 3")
+}
