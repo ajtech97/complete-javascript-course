035bf4950382f6b7a234bb91d23d9885a7fd4e0c
ajay <ajay.l@intellectselect.com>
2026-01-16 09:29:44 +0530

    promise apis

--- 01-Fundamentals-Part-1/final/theory.js
+++ 01-Fundamentals-Part-1/final/theory.js
@@ -730,3 +730,48 @@
 
 // handlePromise()
 // handlePromise1()
+
+
+
+// Promise APIs
+
+const p1 = new Promise((resolve, reject) => {
+    setTimeout(() => resolve("p1 success"), 3000)
+})
+
+
+const p2 = new Promise((resolve, reject) => {
+    setTimeout(() => resolve("p2 success"), 1000)
+    // setTimeout(() => reject("p2 fail"), 1000)
+})
+
+
+const p3 = new Promise((resolve, reject) => {
+    // setTimeout(() => resolve("p3 success"), 2000)
+    setTimeout(() => reject("p3 fail"), 2000)
+})
+
+// Promise.all([p1, p2, p3]).then((res) => {
+//     console.log(res)
+// }).catch((err) => {
+//     console.log(err)
+// })
+
+
+// Promise.allSettled([p1, p2, p3]).then((res) => {
+//     console.log(res)
+// }).catch((err) => {
+//     console.log(err)
+// })
+
+// Promise.race([p1, p2, p3]).then((res) => {
+//     console.log(res)
+// }).catch((err) => {
+//     console.log(err)
+// })
+
+// Promise.any([p1, p2, p3]).then((res) => {
+//     console.log(res)
+// }).catch((err) => {
+//     console.log(err)
+// })
